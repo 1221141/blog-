4f10f16278d31e0be649121367fb4a2364c71253
1221141 <68903047+1221141@users.noreply.github.com>
2023-04-29 18:06:38 +0200

    Update generate_index.rb

--- generate_index.rb
+++ generate_index.rb
@@ -7,12 +7,11 @@ unsafe_loader = ->(string) { YAML.load(string) }
 
 File.open("README.md", "w") do |file|
   file.write("# Index\n\n")
-  file.write("Here are the blog entries in reverse order\n\n")
   Dir.entries(dir_path).each do |file_name|
     next if file_name == '.' || file_name == '..' || file_name == 'assets' # skip current and parent directory links
     
     file_contents = File.read("#{dir_path}/#{file_name}")
     parsed = FrontMatterParser::Parser.new(:md, loader: unsafe_loader).call(file_contents)
-    file.write"[#{parsed.front_matter['Created']} - #{parsed.front_matter['Title']}](#{dir_path}/#{file_name})\n"
+    file.write"- [#{parsed.front_matter['Created']} - #{parsed.front_matter['Title']}](#{dir_path}/#{file_name})\n"
   end
 end
